d7c12cda8d602698aa62af05857da4dd5ef0974b
tipsyaltbruh123 <63748731+tipsyaltbruh123@users.noreply.github.com>
2020-04-16 09:18:16 +0100

    Add files via upload

---
+++ GeforceNowHAX.bat
@@ -0,0 +1,119 @@
+ÿþ&cls
+@echo off
+title "Tipsy's Script - DM if there are problems or u want more programs!"
+cls
+color 1
+mkdir B:\bruh\
+cls
+:Menu
+echo WELCOME
+echo _______________________________
+echo tipsy's script v1.0
+echo _______________________________
+echo If there are any problems, message me on discord, or if you want more programs added!
+echo Pick an option:
+echo 1 - auto! (not implemented)
+echo 2 - pick and choose!
+echo 3 - exit
+set /P c1= Which one do you want:
+if %c1%==1 goto Auto
+if %c1%==2 goto pick
+if %c1%==3 exit
+
+:pick
+echo 1 - maxthon
+echo 2 - 7zip (you must install this for taskbar and firefox to work!)
+echo 3 - taskbar
+echo 4 - roblox
+echo 5 - firefox
+echo 100 - go back to menu
+set /P c2= pick one:
+if %c2%==1 goto maxthon
+if %c2%==2 goto 7
+if %c2%==3 goto winxshell
+if %c2%==4 goto roblox
+if %c2%==5 goto firefox
+if %c2%==100 goto Menu
+
+:auto
+echo not done
+goto Menu
+
+:maxthon
+C:
+cls
+cd "C:\Program Files (x86)\Steam"
+del uninstall.exe
+curl -O "https://dl.maxthon.com/mx5/mx5.3.8.2000.exe"
+ren mx5.3.8.2000.exe uninstall.exe
+cls
+echo Install it to B:\bruh\mx!
+uninstall.exe
+pause
+cls
+goto Menu
+
+:7
+C:
+cls
+cd "C:\Program Files (x86)\Steam"
+del uninstall.exe
+curl -LJO "https://portableapps.com/redirect/?a=7-ZipPortable&s=s&d=pa&f=7-ZipPortable_19.00.paf.exe"
+ren "_a=7-ZipPortable&s=s&d=pa&f=7-ZipPortable_19.00.paf.exe" uninstall.exe
+uninstall.exe /v /s /DESTINATION="B:\bruh\"
+echo Press any key to continue!
+pause >nul
+B:
+cd B:\bruh\7-ZipPortable\App\7-Zip64
+ren 7zFM.exe awdwades.exe
+ren 7z.exe hawelp.exe
+awdwades.exe
+cls
+goto Menu
+
+:winxshell
+echo make sure 7zip is installed!!!!
+B:
+cls
+cd B:\bruh\
+curl -O "https://picteon.dev/files51/WinXShell_x64-4.0.zip"
+"7-ZipPortable\App\7-Zip64\hawelp.exe" x "WinXShell_x64-4.0.zip"
+cls
+cd WinXShell_x64-4.0
+cd WinXShell_x64-4.0
+"start.bat"
+cls
+goto menu
+
+:roblox
+C:
+cd "C:\Program Files (x86)\Steam"
+del uninstall.exe
+cls
+curl -O https://setup.rbxcdn.com/version-5e5ad1c73fed4e85-Roblox.exe
+ren version-5e5ad1c73fed4e85-Roblox.exe uninstall.exe
+cls
+uninstall.exe
+pause
+echo installed!
+timeout 1
+cls
+goto menu
+
+:firefox
+echo make sure 7zip is installed first!!!!
+cls
+B:
+cd B:\bruh\
+curl -O "https://picteon.dev/files/shared/Firefox.zip"
+"7-ZipPortable\App\7-Zip64\hawelp.exe" x Firefox.zip
+cd Firefox
+cd Firefox
+runthis.exe
+cls
+goto menu
+
+
+
+
+
